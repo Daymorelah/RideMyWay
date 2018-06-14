ab12e500af3ecbc961a7c2f407391685b26c966b
--replace-all <demola.hussainin@gmail.com>
2018-06-14 17:58:52 +0100

    [Chore #1055827] Make user prfoile page UX friendly

--- UI/js/profile.js
+++ UI/js/profile.js
@@ -1,27 +1,27 @@
 window.onload = () => {
   
-  let gridItemTwo = document.getElementsByClassName("grid-item-2");
+  let gridItemTwo = document.getElementById("grid-item-2");
 
   document.getElementById("rides-taken").addEventListener('click', showRidesTaken);
   document.getElementById("rides-given").addEventListener('click', showRidesGiven);
 
   function showRidesGiven() {
-    let listOfRidesGiven = `<ul id="rides-given">` ;
-    for(let i = 0; i < 7; i++) {
-      listOfRidesGiven += `<li> <img src="../images/download2.png">Destination: Surulere`
+    let listOfRidesGiven = `<ul>` ;
+    for(let i = 0; i < 5; i++) {
+      listOfRidesGiven += `<li> <img src="../images/download2.png"/>Destination: Surulere`
       +`<br/> Origin: Victoria Island <br/> Time: 7:00 AM</li>`;
     }
     listOfRidesGiven += `</ul>`;
-    gridItemTwo[0].innerHTML = listOfRidesGiven;
+    gridItemTwo.innerHTML = listOfRidesGiven;
   }
   
   function showRidesTaken() {
-    let listOfRidesTaken = `<ul id="rides-given">` ;
-    for(let i = 0; i < 7; i++) {
-      listOfRidesTaken += `<li> <img src="../images/download2.png">Destination: Sabo, `  
+    let listOfRidesTaken = `<ul>` ;
+    for(let i = 0; i < 5; i++) {
+      listOfRidesTaken += `<li> <img src="../images/download2.png" />Destination: Sabo, `  
       + `Yaba <br/> Origin: Opebi, Ikeja <br/> Time: 7:00 AM</li>`;
     }
     listOfRidesTaken += `</ul>`;
-    gridItemTwo[0].innerHTML = listOfRidesTaken;
+    gridItemTwo.innerHTML = listOfRidesTaken;
   }
 } //end of widow.onlaod
