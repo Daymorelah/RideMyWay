605cb62a1c2f0ab10c6f58bbc05db18df8b89f37
--replace-all <demola.hussainin@gmail.com>
2018-06-26 18:55:23 +0100

    [Chore #2178134] Update .gitignore file

--- UI/js/profilePage.js
+++ UI/js/profilePage.js
@@ -6,22 +6,22 @@ window.onload = () => {
   document.getElementById("rides-given").addEventListener('click', showRidesGiven);
 
   function showRidesGiven() {
-    let listOfRidesGiven = `<ul>` ;
+    let listOfRidesGiven = `<div><ul>` ;
     for(let i = 0; i < 5; i++) {
       listOfRidesGiven += `<li> <img src="../images/download2.png"/>Destination: Surulere`
       +`<br/> Origin: Victoria Island <br/> Time: 7:00 AM</li>`;
     }
-    listOfRidesGiven += `</ul>`;
+    listOfRidesGiven += `</ul></div>`;
     gridItemTwo.innerHTML = listOfRidesGiven;
   }
   
   function showRidesTaken() {
-    let listOfRidesTaken = `<ul>` ;
+    let listOfRidesTaken = `<div><ul>` ;
     for(let i = 0; i < 5; i++) {
       listOfRidesTaken += `<li> <img src="../images/download2.png" />Destination: Sabo, `  
       + `Yaba <br/> Origin: Opebi, Ikeja <br/> Time: 7:00 AM</li>`;
     }
-    listOfRidesTaken += `</ul>`;
+    listOfRidesTaken += `</ul></div>`;
     gridItemTwo.innerHTML = listOfRidesTaken;
   }
 } //end of widow.onlaod
