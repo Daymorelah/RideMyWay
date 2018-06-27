01ae72dfcb1e2145c5e16301c700c59c9c428ff0
--replace-all <demola.hussainin@gmail.com>
2018-06-27 23:10:04 +0100

    [Chore #2178134] Make code comply with ESLint

--- UI/js/generalPage.js
+++ UI/js/generalPage.js
@@ -1,21 +1,17 @@
-
+/* eslint-disable no-undef */
 window.onload = () => {
-  let showOffers = document.getElementById("show-offers");
-  let setRideOffer = document.getElementById("set-ride-offer");
-  let searchBy = document.getElementById("search-by");
-  let paginate = document.getElementById("paginate");
+  const showOffers = document.getElementById('show-offers');
+  const setRideOffer = document.getElementById('set-ride-offer');
+  const searchBy = document.getElementById('search-by');
+  const paginate = document.getElementById('paginate');
 
-  document.getElementById("check-ride-offers").addEventListener('click', showRideoffers);;
-  document.getElementById("offer-a-ride").addEventListener('click', showOfferARide);;
-  document.getElementById("check-offered-rides").addEventListener('click', showOfferedRides);;
-  
   function showRideoffers() {
-    let listOfRideOffers = `<ul>` ;
-    for(let i = 0; i < 4; i++) {
-      listOfRideOffers += `<li> <img src="../images/download2.png"/>Destination: Surulere`
-      +`<br/> Origin: Victoria Island <br/> Time: 7:00 AM</li>`;
+    let listOfRideOffers = '<ul>';
+    for (let i = 0; i < 4; i += 1) {
+      listOfRideOffers += '<li> <img src="../images/download2.png"/>Destination: Surulere'
+      + '<br/> Origin: Victoria Island <br/> Time: 7:00 AM</li>';
     }
-    listOfRideOffers += `</ul>`;
+    listOfRideOffers += '</ul>';
     setRideOffer.style.display = 'none';
     showOffers.style.display = 'block';
     searchBy.style.display = 'block';
@@ -24,21 +20,21 @@ window.onload = () => {
   }
 
   function showOfferARide() {
-    showOffers.style.display = "none";
+    showOffers.style.display = 'none';
     setRideOffer.style.display = 'flex';
     searchBy.style.display = 'none';
     paginate.style.display = 'none';
   }
 
   function showOfferedRides() {
-    let acceptButton = `<button>Accept</button>`;
-    let declineButton = `<button>Decline</button>`;
-    let listOfRideOffers = `<ul>` ;
-    for(let i = 0; i < 4; i++) {
-      listOfRideOffers += `<li> <img src="../images/download2.png"/> Agbolahan Adeshola `
-      +`<br/>would like to ride with you ${acceptButton} ${declineButton} </li>`;
+    const acceptButton = '<button>Accept</button>';
+    const declineButton = '<button>Decline</button>';
+    let listOfRideOffers = '<ul>';
+    for (let i = 0; i < 4; i += 1) {
+      listOfRideOffers += '<li> <img src="../images/download2.png"/> Agbolahan Adeshola '
+      + `<br/>would like to ride with you ${acceptButton} ${declineButton} </li>`;
     }
-    listOfRideOffers += `</ul>`;
+    listOfRideOffers += '</ul>';
     setRideOffer.style.display = 'none';
     showOffers.style.display = 'block';
     searchBy.style.display = 'block';
@@ -46,5 +42,9 @@ window.onload = () => {
     showOffers.innerHTML = listOfRideOffers;
   }
 
+  document.getElementById('check-ride-offers').addEventListener('click', showRideoffers);
+  document.getElementById('offer-a-ride').addEventListener('click', showOfferARide);
+  document.getElementById('check-offered-rides').addEventListener('click', showOfferedRides);
   showRideoffers();
-}
+};
+
--- UI/js/profilePage.js
+++ UI/js/profilePage.js
@@ -1,35 +1,34 @@
+/* eslint-disable no-undef */
 window.onload = () => {
-  
-  let gridItemTwo = document.getElementById("ask-user");
-  let searchBy = document.getElementById("search-by");
-  let paginate = document.getElementById("paginate");
-
-  document.getElementById("rides-taken").addEventListener('click', showRidesTaken);
-  document.getElementById("rides-given").addEventListener('click', showRidesGiven);
+  const gridItemTwo = document.getElementById('ask-user');
+  const searchBy = document.getElementById('search-by');
+  const paginate = document.getElementById('paginate');
 
   function showRidesGiven() {
-    searchBy.style.display = "block";
-    paginate.style.display = "block";
-    let listOfRidesGiven = `<div><ul>` ;
-    for(let i = 0; i < 4; i++) {
-      listOfRidesGiven += `<li> <img src="../images/download2.png"/>Destination: Surulere`
-      +`<br/> Origin: Victoria Island <br/> Time: 7:00 AM</li>`;
+    searchBy.style.display = 'block';
+    paginate.style.display = 'block';
+    let listOfRidesGiven = '<div><ul>';
+    for (let i = 0; i < 4; i += 1) {
+      listOfRidesGiven += '<li> <img src="../images/download2.png"/>Destination: Surulere'
+      + '<br/> Origin: Victoria Island <br/> Time: 7:00 AM</li>';
     }
-    listOfRidesGiven += `</ul></div>`;
-    gridItemTwo.style.padding = "0px";
+    listOfRidesGiven += '</ul></div>';
+    gridItemTwo.style.padding = '0px';
     gridItemTwo.innerHTML = listOfRidesGiven;
   }
-  
+
   function showRidesTaken() {
-    searchBy.style.display = "block";
-    paginate.style.display = "block";
-    let listOfRidesTaken = `<div><ul>` ;
-    for(let i = 0; i < 4; i++) {
-      listOfRidesTaken += `<li> <img src="../images/download2.png" />Destination: Sabo, `  
-      + `Yaba <br/> Origin: Opebi, Ikeja <br/> Time: 7:00 AM</li>`;
+    searchBy.style.display = 'block';
+    paginate.style.display = 'block';
+    let listOfRidesTaken = '<div><ul>';
+    for (let i = 0; i < 4; i += 1) {
+      listOfRidesTaken += '<li> <img src="../images/download2.png" />Destination: Sabo, '
+      + 'Yaba <br/> Origin: Opebi, Ikeja <br/> Time: 7:00 AM</li>';
     }
-    listOfRidesTaken += `</ul></div>`;
-    gridItemTwo.style.padding = "0px";
+    listOfRidesTaken += '</ul></div>';
+    gridItemTwo.style.padding = '0px';
     gridItemTwo.innerHTML = listOfRidesTaken;
   }
-} //end of widow.onlaod
+  document.getElementById('rides-taken').addEventListener('click', showRidesTaken);
+  document.getElementById('rides-given').addEventListener('click', showRidesGiven);
+}; // end of widow.onlaod
