a000e7d21406afe4f6789961365decace9bd21b4
--replace-all <demola.hussainin@gmail.com>
2018-06-27 22:55:10 +0100

    [Chore #2178134] Implement feedback in UI template

--- UI/js/generalPage.js
+++ UI/js/generalPage.js
@@ -2,40 +2,47 @@
 window.onload = () => {
   let showOffers = document.getElementById("show-offers");
   let setRideOffer = document.getElementById("set-ride-offer");
+  let searchBy = document.getElementById("search-by");
+  let paginate = document.getElementById("paginate");
 
-  document.getElementById("check-ride-offers").addEventListener('click', showRideoffers);
-  document.getElementById("offer-a-ride").addEventListener('click', showOfferARide);
-  document.getElementById("check-offered-rides").addEventListener('click', showOfferedRides);
-
+  document.getElementById("check-ride-offers").addEventListener('click', showRideoffers);;
+  document.getElementById("offer-a-ride").addEventListener('click', showOfferARide);;
+  document.getElementById("check-offered-rides").addEventListener('click', showOfferedRides);;
+  
   function showRideoffers() {
     let listOfRideOffers = `<ul>` ;
-    for(let i = 0; i < 5; i++) {
+    for(let i = 0; i < 4; i++) {
       listOfRideOffers += `<li> <img src="../images/download2.png"/>Destination: Surulere`
       +`<br/> Origin: Victoria Island <br/> Time: 7:00 AM</li>`;
     }
     listOfRideOffers += `</ul>`;
     setRideOffer.style.display = 'none';
     showOffers.style.display = 'block';
+    searchBy.style.display = 'block';
+    paginate.style.display = 'block';
     showOffers.innerHTML = listOfRideOffers;
   }
 
   function showOfferARide() {
     showOffers.style.display = "none";
-    setRideOffer.style.display = 'block';
+    setRideOffer.style.display = 'flex';
+    searchBy.style.display = 'none';
+    paginate.style.display = 'none';
   }
 
   function showOfferedRides() {
-    let acceptButton = `<input type='submit' value='accept' />`;
-    let declineButton = `<input type='submit' value='decline' />`;
+    let acceptButton = `<button>Accept</button>`;
+    let declineButton = `<button>Decline</button>`;
     let listOfRideOffers = `<ul>` ;
-    for(let i = 0; i < 5; i++) {
+    for(let i = 0; i < 4; i++) {
       listOfRideOffers += `<li> <img src="../images/download2.png"/> Agbolahan Adeshola `
-      +`<br/>would like to ride with you <div id='choose-offer-container'>${acceptButton}`
-      +`${declineButton}</div></li>`;
+      +`<br/>would like to ride with you ${acceptButton} ${declineButton} </li>`;
     }
     listOfRideOffers += `</ul>`;
     setRideOffer.style.display = 'none';
     showOffers.style.display = 'block';
+    searchBy.style.display = 'block';
+    paginate.style.display = 'block';
     showOffers.innerHTML = listOfRideOffers;
   }
 
