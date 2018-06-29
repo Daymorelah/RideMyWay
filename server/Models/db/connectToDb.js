deecd48a7d5900d801e18bf6fc1cc82c0f574bd6
--replace-all <demola.hussainin@gmail.com>
2018-06-29 05:26:46 +0100

    [Bug #2178134] Try to fix bug preventing heroku from seeing the database

--- server/Models/db/connectToDb.js
+++ server/Models/db/connectToDb.js
@@ -6,9 +6,9 @@ let pool;
 console.log(`We are in the ${process.env.NODE_ENV} environment`);
 
 if (process.env.DATABASE_URL) {
-  pool = new Pool(process.env.DATABASE_URL);
+  pool = new Pool({ connectionString: process.env.DATABASE_URL });
 } else {
-  pool = new Pool(process.env.DATABASE_URL);
+  pool = new Pool({ connectionString: process.env.DATABASE_URL });
 }
 
 const connectToDb = (text, params, callback) => pool.query(text, params, callback);
