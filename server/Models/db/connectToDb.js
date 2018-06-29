42f05ff74dc74086d099c37dc10540336db36fb8
--replace-all <demola.hussainin@gmail.com>
2018-06-29 06:20:24 +0100

    [Chore #2178134] Setup test environment

--- server/Models/db/connectToDb.js
+++ server/Models/db/connectToDb.js
@@ -6,8 +6,12 @@ let pool;
 
 if (process.env.DATABASE_URL) {
   pool = new Pool({ connectionString: process.env.DATABASE_URL });
-} else {
+}
+if (process.env.NODE_ENV === 'development') {
   pool = new Pool(config.development);
+}
+if (process.env.NODE_ENV === 'test') {
+  pool = new Pool(config.test);
 }
 
 const connectToDb = (text, params, callback) => pool.query(text, params, callback);
